b97c7ade281fa32898b7440b4984d48d0e4d9d48
MeltedPenguin <diablo22007@gmail.com>
2017-02-26 13:01:21 +0100

    Remove JQuery dependency and Add new provider

--- script.js
+++ script.js
@@ -6,7 +6,6 @@
 // @author       Eddydg
 // @match        https://www.betaseries.com/membre/*/episodes
 // @grant        GM_xmlhttpRequest
-// @require      http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
 (function() {
@@ -15,40 +14,58 @@
     let providers = {
         "idope": {
             getUrl: (q) => `https://idope.se/torrent-list/${encodeURIComponent(q)}`,
-            hashId: "#hideinfohash0"
+            getHash: (dom) => {
+                return dom.find("#hideinfohash0").text();
+            }
+        },
+        "skytorrents": {
+            getUrl: (q) => `https://www.skytorrents.in/search/all/ed/1/?l=en-us&q=${encodeURIComponent(q)}`,
+            getHash: (dom) => {
+                return dom.querySelector("tbody tr a[href^='magnet']").href;
+            }
         }
     };
 
     function getBtn(link, name) {
-        return `<div class="markas"><a class="markas_img" href="${link}">${name}</a></div>`;
+        let btn = document.createElement('div');
+        btn.className = "markas";
+
+        let a = document.createElement('a');
+        a.className = "markas_img";
+        a.href = link;
+        a.innerHTML = name;
+
+        btn.appendChild(a);
+
+        return btn;
     }
 
     var observer = new MutationObserver(function(mutations) {
         observer.disconnect();
-        let aEpisode = $("#episodes_container .episodes .episode");
-        var episodes = document.querySelectorAll('#episodes_container .episode-titre');
+        var episodes = document.querySelectorAll('#episodes_container .episode');
         for (let i = 0; i < episodes.length; i++) {
             let position = 0;
 
-            let showName = episodes[i].querySelector('a:nth-child(1)').firstChild.nodeValue;
-            let showEpisode = episodes[i].querySelector('a:nth-child(2)').firstChild.nodeValue;
+            let showName = episodes[i].querySelector('.episode-titre a:nth-child(1)').firstChild.nodeValue;
+            let showEpisode = episodes[i].querySelector('.episode-titre a:nth-child(2)').firstChild.nodeValue;
             let query = showName + " " + showEpisode + " x265";
 
-            let provider = providers["idope"];
+            let provider = providers["skytorrents"];
             GM_xmlhttpRequest({
                 method: "GET",
                 url: provider.getUrl(query),
                 fetch: true,
                 onreadystatechange: function(state) {
-                    let dom = $(state.responseText);
-                    let magnetHash = dom.find(provider.hashId).text();
+                    let parser = new DOMParser();
+                    let dom = parser.parseFromString(state.responseText, "text/html");
 
+                    let magnetHash = provider.getHash(dom);
                     if (!magnetHash) return;
 
                     let magnet = `magnet:?xt=urn:btih:${magnetHash}`;
 
                     let btn = getBtn(magnet, "1080p");
-                    $(aEpisode[i]).find(".episode-side").prepend(btn);
+                    episodes[i].querySelector(".episode-side").prepend(btn);
                 }
             });
         }
