27214ce721a78709072f5b47beb826ebda1529b9
MeltedPenguin <diablo22007@gmail.com>
2017-02-28 17:46:54 +0100

    Retrieve all results' magnets instead of only first one

--- script.js
+++ script.js
@@ -15,16 +15,16 @@
     let providers = {
         "idope": {
             getUrl: (q) => `https://idope.se/torrent-list/${encodeURIComponent(q)}`,
-            getLink: (dom) => {
-                let hash = dom.querySelector("#hideinfohash0").innerHTML;
-                if (!hash) return "";
-                return `magnet:?xt=urn:btih:${hash}`;
+            getLinks: (dom) => {
+                return [...dom.querySelectorAll("div[id^='hideinfohash']")]
+                    .map(e => `magnet:?xt=urn:btih:${e.innerHTML}`);
             }
         },
         "skytorrents": {
             getUrl: (q) => `https://www.skytorrents.in/search/all/ed/1/?l=en-us&q=${encodeURIComponent(q)}`,
-            getLink: (dom) => {
-                return dom.querySelector("tbody tr a[href^='magnet']").href;
+            getLinks: (dom) => {
+                return [...dom.querySelectorAll("tbody tr a[href^='magnet']")]
+                    .map(a => a.href);
             }
         }
     };
@@ -40,8 +40,8 @@
         img.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACsElEQVQ4T23QbUhTYRQH8P+9dvMtcznYLHWkU6cLArtNm2FaFEWZRkSkUYq2jBANti+BiVDSF5OMjJQZiLQiYiRpEEVpI5e2pcbUjTWNqdMNW2m+dLN248oac+yB58Phf86P5zkEAk4nEMUAx1ngMAEkcTELjBPAy1DgeSHw03+E8C86gK0USTaI5PSeJHmmIDKaF8HlS/M/lsf1Ay673vhh1eNRnQNm/s/5gDYgKpQkW3adLjwgTBAJfzm/r3tbmHALnJN256cnnW8Yj6ei3PsSH9AOFCfK6etiWWbSwpTTMzI66piw292ckigSxeyQSrdtjheSto8D4xN647USQMNlPuAB0J5zqeTk0rR706jZPGWxWh9RQBPXtApUS1JSiqRpafGRcTGLuvvt2jKgZB2gBnR5l8uzXZavZG9f3+e/KytHa4BprukGEBcSHv4iNzt7p0Cy3dNzr63vApCzDmgBDPsU5+kZkxXv9HpjHbDbfwl1XC6X07HSZOjaOowV3tz3hbuAIbf4FD07ZsP7wcGgwN6MDDo2XYxezVNjZSBwGzAcPHGM/jb2BW8tlqDAfomE5qcn4/WzbuOVQKABMBQeyqPnzTZ0TU4GBfITEujoNDE6X/UYVYHATcBwNkdOL1tseOxyBQXOCAR0hESMhzq98WogIJPJFGq1utVsdqGxsUbV399/y3+JWVlZF5ubm1scjhXU16t8uW+JYrFYodVq14Da2spqi8Vyxx9ITU1VaDSaVg5QKsuqrVbrWu4DeDxelclkahoasqO0tEA5NzfX6A/w+fyq4eHhppERB4qKjijdbvdazgE8AFEUReWzLCskSZIgCGKWYZhuAIsA/gCIpCiqgGXZWJIkQwiCmGEYpgvAAgeEAQj13g0APN6hVQC/vfVGANylAJDenAHA/AMeSRE3vrDTDwAAAABJRU5ErkJggg==";
 
         a.appendChild(img);
-        //btn.appendChild(a);
-        btn.appendChild(img);
+        btn.appendChild(a);
+        //btn.appendChild(img);
 
         btn.onclick = ()  => {
             let magnetBox = document.querySelector(`#${episodeId}`);
@@ -95,7 +95,7 @@
                     let parser = new DOMParser();
                     let dom = parser.parseFromString(state.responseText, "text/html");
 
-                    let link = provider.getLink(dom);
+                    let link = provider.getLinks(dom)[0];
                     if (!link) return;
 
                     let episodeId = (showName + showEpisode).replace(/[^a-z0-9]/gi,'');
