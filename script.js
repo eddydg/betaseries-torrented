82bdb9da402494d84d7f02bf0e85eda7516bc547
MeltedPenguin <diablo22007@gmail.com>
2017-03-02 19:57:37 +0100

    Some more cleaning

--- script.js
+++ script.js
@@ -15,25 +15,25 @@
         "idope": {
             getUrl: (q) => `https://idope.se/torrent-list/${encodeURIComponent(q)}/?&c=3`,
             getLinks: (dom) => {
-                return [...dom.querySelectorAll(".resultdiv")]
-                    .map(e => { return {
+                [...dom.querySelectorAll(".resultdiv")]
+                    .map(e => ({
                         title: e.querySelector(".resultdivtopname").innerHTML.trim(),
                         link: `magnet:?xt=urn:btih:${e.querySelector("div[id^='hideinfohash']").innerHTML}`,
                         size: e.querySelector(".resultdivbottonlength").innerHTML.trim()
-                    };
-                });
+                    })
+                );
             }
         },
         "skytorrents": {
             getUrl: (q) => `https://www.skytorrents.in/search/all/ed/1/?l=en-us&q=${encodeURIComponent(q)}`,
             getLinks: (dom) => {
-                return [...dom.querySelectorAll("tbody tr")]
-                    .map(tr => { return {
+                [...dom.querySelectorAll("tbody tr")]
+                    .map(tr => ({
                         title: tr.querySelector("a").innerHTML.trim(),
                         link: tr.querySelector("a[href^='magnet']").href,
                         size: tr.querySelector("td:nth-child(2)").innerHTML.trim()
-                    };
-                });
+                    })
+                );
             }
         }
     };
@@ -48,8 +48,7 @@
         img.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACsElEQVQ4T23QbUhTYRQH8P+9dvMtcznYLHWkU6cLArtNm2FaFEWZRkSkUYq2jBANti+BiVDSF5OMjJQZiLQiYiRpEEVpI5e2pcbUjTWNqdMNW2m+dLN248oac+yB58Phf86P5zkEAk4nEMUAx1ngMAEkcTELjBPAy1DgeSHw03+E8C86gK0USTaI5PSeJHmmIDKaF8HlS/M/lsf1Ay673vhh1eNRnQNm/s/5gDYgKpQkW3adLjwgTBAJfzm/r3tbmHALnJN256cnnW8Yj6ei3PsSH9AOFCfK6etiWWbSwpTTMzI66piw292ckigSxeyQSrdtjheSto8D4xN647USQMNlPuAB0J5zqeTk0rR706jZPGWxWh9RQBPXtApUS1JSiqRpafGRcTGLuvvt2jKgZB2gBnR5l8uzXZavZG9f3+e/KytHa4BprukGEBcSHv4iNzt7p0Cy3dNzr63vApCzDmgBDPsU5+kZkxXv9HpjHbDbfwl1XC6X07HSZOjaOowV3tz3hbuAIbf4FD07ZsP7wcGgwN6MDDo2XYxezVNjZSBwGzAcPHGM/jb2BW8tlqDAfomE5qcn4/WzbuOVQKABMBQeyqPnzTZ0TU4GBfITEujoNDE6X/UYVYHATcBwNkdOL1tseOxyBQXOCAR0hESMhzq98WogIJPJFGq1utVsdqGxsUbV399/y3+JWVlZF5ubm1scjhXU16t8uW+JYrFYodVq14Da2spqi8Vyxx9ITU1VaDSaVg5QKsuqrVbrWu4DeDxelclkahoasqO0tEA5NzfX6A/w+fyq4eHhppERB4qKjijdbvdazgE8AFEUReWzLCskSZIgCGKWYZhuAIsA/gCIpCiqgGXZWJIkQwiCmGEYpgvAAgeEAQj13g0APN6hVQC/vfVGANylAJDenAHA/AMeSRE3vrDTDwAAAABJRU5ErkJggg==";
 
         btn.appendChild(img);
-
-        btn.onclick = ()  => {
+        btn.onclick = () => {
             let magnetBox = document.querySelector(`#${episodeId}`);
             if (magnetBox.style.display === "none") {
                 magnetBox.style.display = "";
@@ -92,6 +91,7 @@
       ul.appendChild(li);
     }
 
+    let domParser = new DOMParser();
     var observer = new MutationObserver(function(mutations) {
         observer.disconnect(); // Avoid endless loop
 
@@ -106,9 +106,7 @@
                 url: provider.getUrl(query),
                 fetch: true,
                 onreadystatechange: function(state) {
-                    let parser = new DOMParser();
-                    let dom = parser.parseFromString(state.responseText, "text/html");
-
+                    let dom = domParser.parseFromString(state.responseText, "text/html");
                     let results = provider.getLinks(dom);
                     if (results.length < 1) return;
 
